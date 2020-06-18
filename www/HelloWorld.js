e7a3b329fa8d4be64b477cb9b0b6bfa31741d11e
Blerim Cazimi <blerim@burd.dk>
2020-06-18 15:08:32 +0200

    Fucking Bridge

--- www/HelloWorld.js
+++ www/HelloWorld.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.nativeToast = function (arg0, success, error) {
     exec(success, error, 'HelloWorld', 'nativeToast', [arg0]);
 };
+
+exports.getProcessing = function (arg0, success, error) {
+    exec(success, error, 'HelloWorld', 'getProcessing', [arg0]);
+};
